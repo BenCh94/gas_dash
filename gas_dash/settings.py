7eb33278ca3a6194771dcb4d3e96841f30250886
Ben <bchadwick94@gmail.com>
2018-08-18 20:36:09 +0100

    static folder

--- gas_dash/settings.py
+++ gas_dash/settings.py
@@ -124,11 +124,11 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/gas_dash/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'gas_dash/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 # Simplified static file serving.
