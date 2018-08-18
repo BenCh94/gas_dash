f54b46616c2d50ea72a384055dfa30195ff138dd
Ben <bchadwick94@gmail.com>
2018-08-18 20:40:30 +0100

    remove settings

--- gas_dash/settings.py
+++ gas_dash/settings.py
@@ -124,12 +124,12 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = [
+#     os.path.join(PROJECT_ROOT, 'static'),
+# ]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
