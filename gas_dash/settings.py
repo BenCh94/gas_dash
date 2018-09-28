2a81b9a7de9ce61c1e5638d75978c83babefd223
Ben <bchadwick94@gmail.com>
2018-09-28 19:34:44 +0100

    adding db backend

--- gas_dash/settings.py
+++ gas_dash/settings.py
@@ -116,7 +116,7 @@ WSGI_APPLICATION = 'gas_dash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresl_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gaslocaldb',
         'USER': 'dbadmin',
         'PASSWORD': os.environ.get('db_pass'),
