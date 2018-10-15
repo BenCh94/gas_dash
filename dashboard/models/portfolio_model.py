26bed1e98a646f4baca8bd67e6dfe1ace1fc9d2d
Ben <bchadwick94@gmail.com>
2018-10-15 01:07:31 +0100

    graph settings and benchmark init

--- dashboard/models/portfolio_model.py
+++ dashboard/models/portfolio_model.py
@@ -1,15 +1,21 @@
 import datetime
-
+import json
 from django.db import models
 from django.utils import timezone
 from django.contrib.postgres.fields import JSONField
 
 class Portfolio(models.Model):
 	user_profile = models.ForeignKey('dashboard.Profile', on_delete=models.CASCADE)
+	benchmark = models.ForeignKey('dashboard.Benchmark', on_delete=models.SET_NULL, null=True)
 	data = JSONField()
 	name = models.CharField(max_length=200)
 	def __str__(self):
 		return self.name
+
+	def current_gain(self):
+		port_array = json.loads(self.data)
+		print(list(port_array))
+		return port_array
 
 	class Meta:
 		unique_together = ('user_profile', 'name')
