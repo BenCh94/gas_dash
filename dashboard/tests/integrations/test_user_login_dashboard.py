b6304014da631725cd52c98ff12f19e967ff36a3
Ben <bchadwick94@gmail.com>
2018-11-11 23:42:47 +0000

    adding sauce labs for travis ci

--- dashboard/tests/integrations/test_user_login_dashboard.py
+++ dashboard/tests/integrations/test_user_login_dashboard.py
@@ -1,4 +1,11 @@
 # tests valid user can login to the dashboard (uses selenium)
+import os
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 from django.shortcuts import get_object_or_404
 from django.test.utils import override_settings
 from ...models import Stock, Trade, Profile, User
@@ -6,12 +13,23 @@ from ..factories import UserFactory, StockFactory, TradeFactory
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase, LiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+desired_cap = {
+    'platform': "Mac OS X 10.12",
+    'browserName': "chrome",
+    'version': "latest",
+}
+username = os.environ['SAUCE_USERNAME']
+access_key = os.environ['SAUCE_ACCESS_KEY']
+
 class TestLogin(LiveServerTestCase):
     
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.selenium = WebDriver('/home/ben/path_executable/chromedriver')
+        if 'TRAVIS' in os.environ:
+            cls.selenium = webdriver.Remote(command_executor='https://{}:{}@ondemand.saucelabs.com/wd/hub'.format(username, access_key), desired_capabilities=desired_cap)
+        else:
+            cls.selenium = WebDriver('/home/ben/path_executable/chromedriver')
         cls.selenium.implicitly_wait(10)
         user = UserFactory.create(username='login_user')
         user.set_password('test12345')
