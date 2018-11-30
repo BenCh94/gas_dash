e3f7140de76e422043bc82a8eb22b4cea170a269
Ben <bchadwick94@gmail.com>
2018-11-30 20:09:02 +0000

    set integration tests to run on chrome headless in travis

--- dashboard/tests/integrations/test_user_login_dashboard.py
+++ dashboard/tests/integrations/test_user_login_dashboard.py
@@ -21,6 +21,7 @@ class TestLogin(LiveServerTestCase):
         super().setUpClass()
         if 'TRAVIS' in os.environ:
             chrome_options = webdriver.ChromeOptions()
+            chrome_options.set_binary('/usr/local/share/chromedriver')
             chrome_options.add_argument('--no-snadbox')
             chrome_options.add_argument('--window-size=1420,1080')
             chrome_options.add_argument('--headless')
