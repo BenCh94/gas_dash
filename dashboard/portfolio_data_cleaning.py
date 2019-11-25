ca83868b22d1f8a72d55e16f1eac10aebf0f7f2b
Ben <bchadwick94@gmail.com>
2019-11-25 00:06:02 +0000

    error applying benchmark data

--- dashboard/portfolio_data_cleaning.py
+++ dashboard/portfolio_data_cleaning.py
@@ -28,6 +28,31 @@ def add_sell(trade, df):
 	df = df.append([trade], ignore_index=True)
 	return df
 
+def apply_trade_data(df, trade):
+	df['amount'] = trade['amount']
+	df['fees_usd'] = trade['fees_usd']
+	df['invested'] = trade['invested']
+	df['value'] = df['close']*df['amount']
+	df['gain'] = df['value'] - df['invested']
+	df['gain_pct'] = (df['gain']/df['invested'])*100
+	return df
+
+def apply_benchmark(df, bench_chart, trade, end_date):
+	if end_date != '':
+		bench_chart['date'] = pd.to_datetime(bench_chart['date'])
+		mask = (bench_chart['date'] >= trade['date']) & (bench_chart['date'] < end_date)
+		bench_prices = bench_chart.loc[mask]
+		df['bench_value'] = bench_prices['close'].apply(lambda x: x*0.3)
+		df['bench_gain'] = df['bench_value'] - df['invested']
+		df['bench_gain_pct'] = (df['bench_gain']/df['invested'])*100
+	else:
+		bench_chart['date'] = pd.to_datetime(bench_chart['date'])
+		bench_prices = bench_chart[bench_chart['date'] >= trade['date']]
+		df['bench_value'] = bench_prices['close']*trade['benchmark_amount']
+		df['bench_gain'] = df['bench_value'] - df['invested']
+		df['bench_gain_pct'] = (df['bench_gain']/df['invested'])*100
+	return df
+
 
 class PortfolioUpdate():
 	""" Object for updating a users portfolio data """
@@ -52,6 +77,7 @@ class PortfolioUpdate():
 			time_queries = {0: '6m', 1: '2y', 2: '5y', 3: '5y'}
 			date_range = time_queries[int(time_diff.days/365)]
 		day_chart = request_chart_from_date(date_range, self.portfolio.benchmark_ticker)
+		self.portfolio.benchmark_data = day_chart
 		return day_chart
 
 	def combine_trades(self, stock):
@@ -85,5 +111,23 @@ class PortfolioUpdate():
 	def apply_historical_prices(self, trade_df, stock):
 		""" Apply trades to historical prices for performance data """
 		price_data = pd.DataFrame(stock.ticker_data.historical_data['chart'])
-		print(price_data)
-		# NOW have trades and pricing data last step is combining the two into a unified dataframe showing performance over time.
+		price_data['date'] = pd.to_datetime(price_data['date'])
+		bench_chart = pd.DataFrame(self.portfolio.benchmark_data)
+		bench_chart['date'] = pd.to_datetime(bench_chart['date'])
+		frames = []
+		for index, row in trade_df.iterrows():
+			if index < trade_df.index.max():
+				end_date = trade_df['date'].iloc[index+1]
+				mask = (price_data['date'] >= row['date']) & (price_data['date'] < end_date)
+				price_df = price_data.loc[mask]
+				gain_df = apply_trade_data(price_df, row)
+				gain_df = apply_benchmark(gain_df, bench_chart, row, end_date)
+				frames.append(gain_df)
+			else:
+				price_df = price_data[price_data['date'] >= row['date']]
+				gain_df = apply_trade_data(price_df, row)
+				gain_df = apply_benchmark(gain_df, bench_chart, row, '')
+				frames.append(gain_df)
+		full_df = pd.concat(frames)
+		print(full_df[['gain', 'gain_pct', 'value', 'bench_value', 'bench_gain', 'bench_gain_pct']])
+		return full_df
