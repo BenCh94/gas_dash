fcbdeb822974d94ec24bb7a3a8006dae013513ba
Ben <bchadwick94@gmail.com>
2020-02-23 21:15:29 +0000

    collapsable menu added

--- staticfiles/js/dashboard.js
+++ staticfiles/js/dashboard.js
@@ -21,4 +21,22 @@ $(document).ready(function(){
 	$('#graph_timelines').on('click', function(){
 		$('#time_btns').show()
 	})
+
+	$("#closeMenu").click(function(){
+		$('.sidebar').css('width', '3vw');
+		$('.main').css('width', '95vw');
+		$('#sidebar_nav').hide();
+		$('#sidebar_icons').show();
+		$('#iex_attr').hide();
+		$(this).hide();
+	})
+
+	$('#openMenu').click(function(){
+		$('.sidebar').css('width', '12vw');
+		$('.main').css('width', '85vw');
+		$('#sidebar_nav').show();
+		$('#sidebar_icons').hide();
+		$('#iex_attr').show();
+		$('#closeMenu').show();
+	})
 })
