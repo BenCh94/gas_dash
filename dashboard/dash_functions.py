ac4212839db1a903128c188f50bc5f3256609267
Ben <bchadwick94@gmail.com>
2020-02-23 20:45:54 +0000

    show error if portfolio data is empty

--- dashboard/dash_functions.py
+++ dashboard/dash_functions.py
@@ -4,9 +4,9 @@ from .stock_functions import get_current_value
 
 def get_latest_data(portfolio, stocks):
 	""" Get the last days data for stocks and portfolio """
-	if portfolio:
+	if portfolio and portfolio.latest_day_data() != 'Empty portfolio response':
 		latest = portfolio.latest_day_data()
 		current_value = get_current_value(stocks, latest)
 		latest = {**latest, **current_value}
 		return latest
-	return ''
+	return 'Portfolio error, please refresh your data.'
