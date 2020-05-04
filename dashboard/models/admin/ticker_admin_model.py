156be1820ec7da0579cbe684d983de41a48631e7
Ben <bchadwick94@gmail.com>
2020-05-04 19:11:33 +0100

    adding search by ids to ticker admin

--- dashboard/models/admin/ticker_admin_model.py
+++ dashboard/models/admin/ticker_admin_model.py
@@ -5,4 +5,4 @@ from .. import Ticker
 class TickerAdmin(admin.ModelAdmin):
     """ Admin site settings for ticker model """
     list_display = ('name', 'ticker', 'reference_date', 'created_at')
-    search_fields = ('name', 'ticker')
+    search_fields = ('name', 'ticker', 'cik', 'figi', 'provider_id')
