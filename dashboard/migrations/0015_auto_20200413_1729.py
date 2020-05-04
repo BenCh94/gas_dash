e10755769df841cff88cfb9cf81fe132c9b899f0
Ben <bchadwick94@gmail.com>
2020-05-04 19:27:19 +0100

    remove unique cik

--- dashboard/migrations/0015_auto_20200413_1729.py
+++ dashboard/migrations/0015_auto_20200413_1729.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='ticker',
             name='cik',
-            field=models.CharField(max_length=50, null=True, unique=True),
+            field=models.CharField(max_length=50, null=True),
         ),
         migrations.AlterField(
             model_name='ticker',
